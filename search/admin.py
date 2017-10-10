dd9d8dd853e544c7d2518ec6a14d7d10abedfaca
ca72ss <2garin7@gmail.com>
2017-10-10 21:21:45 +0300

    Added photo and names

--- search/admin.py
+++ search/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Person
+from .models import Post
 
 admin.site.register(Person)
+admin.site.register(Post)
--- search/views.py
+++ search/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+import vk
 
-def authorization(request):
-    return render(request, 'search/authorization.html', {})
+
+def search(request):
+    at = '72d9d403e83d27930eabaa1d4e50e16ec394ee69881bd2715fe04b5178b434bd43b2cc3db8d29cdd14b4f'
+    session = vk.Session(access_token= at)
+    api = vk.API(session)
+    users = api.users.search(count=10,  age_from=14, age_to=18, school_year=2017,  fields = "photo_100,last_seen,photo_id,has_mobile,universities")
+    del users[0]
+
+    return render(request, 'search/done.html', {'users':users})
