96d585304dd770779d2dc4cc1555e759f3bf10ca
ca72ss <2garin7@gmail.com>
2017-10-18 21:25:47 +0300

    Added search in groups

--- search/urls.py
+++ search/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^$', views.search, name='search'),
     url(r'^forms$', views.forms, name='forms'),
     url(r'^get_name$', views.get_name, name='get_name'),
-    
+    url(r'^getgroupusers$', views.getGroupUsers, name='getGroupUsers'),
 ]
--- search/views.py
+++ search/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import vk
+from django.contrib.sessions.backends.db import SessionStore
+
+s = SessionStore()
+s ['at'] = '371aacf18130dc370fa8d1a9f4c33125208b2a9e84e6abd26a16aa5d412dc8174fbab26a001f55f1d024f'
 
 def search(request):
-    at = '72d9d403e83d27930eabaa1d4e50e16ec394ee69881bd2715fe04b5178b434bd43b2cc3db8d29cdd14b4f'
-    session = vk.Session(access_token= at)
+
+    session = vk.Session(access_token= s ['at'])
     api = vk.API(session)
     users = api.users.search(count=100,  age_from=14, age_to=18, school_year=2017,  fields = "photo_100,last_seen,photo_id,has_mobile,universities")
     groups = api.groups.getMembers(group_id='142498173')
@@ -19,8 +23,35 @@ def forms(request):
 
 
 def get_name(request):
+    session = vk.Session(access_token= s ['at'])
+    api = vk.API(session)
+
+    city_name = 'Шостка'
+    school_num = 'школа 5'
+    UA_CID = api.database.getCountries(code='UA')[0]['cid']
+    CITY = api.database.getCities(country_id=UA_CID, q=city_name)
+    nc = 0
+    CITY_ID = CITY[nc]['cid']
+    SCHOOL =  api.database.getSchools(q=school_num,city_id=CITY_ID)
+    SCHOOL_ID = SCHOOL[1]['id']
+
+
     if 'your_name' in request.GET and request.GET['your_name']:
         your_name = request.GET['your_name']
-        return render(request, 'search/test.html', {'your_name': your_name})
+
+
+        users = api.users.search(count=10,city=CITY_ID, country=UA_CID, age_from=14, age_to=24, school_year=2012, school = SCHOOL_ID, fields = "photo_100,last_seen,photo_id,has_mobile,universities,last_seen,photo_id,has_mobile,universities,can_write_private_message,can_send_friend_request")
+        groups = api.groups.getMembers(group_id='142498173')
+        print (groups)
+        del users[0]
+        return render(request, 'search/done.html', {'your_name': your_name, 'users':users})
     else:
         return HttpResponse('Please submit a search term.')
+
+def getGroupUsers(request):
+    session = vk.Session(access_token= s ['at'])
+    api = vk.API(session)
+    groupsUsers = api.groups.getMembers(group_id = '127378018',fields = "photo_100,last_seen,photo_id,has_mobile,universities,last_seen,photo_id,has_mobile,universities,can_write_private_message,can_send_friend_request")
+    users = groupsUsers['users']
+    print (groupsUsers)
+    return render(request, 'search/done.html', {'users':users})
